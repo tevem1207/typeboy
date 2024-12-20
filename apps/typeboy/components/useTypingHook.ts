b02c56a2550742103dd5d9b44396c25ae63066dd
Taehee_Hwang <tevem1207@gmail.com>
2024-12-20 11:01:21 +0900

    [feat] 에디터 상태 체크 인터벌 추가

--- apps/typeboy/components/useTypingHook.ts
+++ apps/typeboy/components/useTypingHook.ts
@@ -1,43 +1,49 @@
-import { ChangeEventHandler, useState } from "react";
+import { ChangeEventHandler, useEffect, useMemo, useState } from "react";
 import { disassemble } from "es-hangul";
 
 export const useTypingHook = (text: string) => {
   const [inputText, setInputText] = useState("");
-  const [startTime, setStartTime] = useState<Date>();
+  const [elapsedTime, setElapsedTime] = useState(0);
 
-  const elapsedTime = startTime
-    ? new Date().getTime() - startTime.getTime()
-    : 0;
+  const cpm =
+    elapsedTime > 1000
+      ? disassemble(inputText).length / (elapsedTime / 60000)
+      : 0;
 
-  const cpm = disassemble(inputText).length / (elapsedTime / 60000);
-
-  const accuracy =
-    (inputText
-      .slice(0, -1)
-      .split("")
-      .filter((char, index) => {
-        console.log(char, text[index]);
-        return char === text[index];
-      }).length /
-      inputText.slice(0, -1).length) *
-    100;
-
+  const accuracy = useMemo(
+    () =>
+      (inputText
+        .slice(0, -1)
+        .split("")
+        .filter((char, index) => {
+          return char === text[index];
+        }).length /
+        inputText.slice(0, -1).length) *
+      100,
+    [inputText],
+  );
   const handleInputChange: ChangeEventHandler<HTMLInputElement> = (e) => {
     const text = e.target.value;
     setInputText(text);
-    if (!startTime) {
-      setStartTime(new Date());
+    if (!elapsedTime) {
+      setElapsedTime(1);
     }
   };
 
   const handleReset = () => {
     setInputText("");
-    setStartTime(new Date());
+    setElapsedTime(0);
   };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setElapsedTime((prev) => (prev ? prev + 1000 : prev));
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
 
   return {
     inputText,
-    startTime,
     elapsedTime,
     handleInputChange,
     handleReset,
